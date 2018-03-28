a0f13b4be4b60eed1d659cb9581809831e27a370
Jucong <hejucong1995@gmail.com>
2018-03-28 00:10:56 -0400

    random forest done

--- Submission/Code/train.py
+++ Submission/Code/train.py
@@ -2,10 +2,10 @@ import os
 import numpy as np
 import time
 import pickle
+import matplotlib.pyplot as plt
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.linear_model import LogisticRegression
+from collections import defaultdict
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from Library.kaggle import *
@@ -63,31 +63,6 @@ print('finished reading training data')
 print('testing_X data shape is ', X_test.shape)
 print('Testing_Y shape is ', Y_test.shape)
 
-classfiers = [RandomForestClassifier(), LogisticRegression(), SVC()]
-best_pred_score = 0
-best_classifer = None
-
-for c in classfiers:
-    print('training ', c)
-    train_then = time.time()
-    c.fit(X_train, Y_train)
-    train_now = time.time()
-    diff = train_now - train_then
-    print('total training time is {} minute, {} second'.format(diff // 60, diff % 60))
-
-    pred_then = time.time()
-    y_pred = c.predict(X_test)
-    pred_now = time.time()
-    diff = pred_now - pred_then
-    print('total training time is {} minute, {} second'.format(diff // 60, diff % 60))
-
-    score = accuracy_score(Y_test, y_pred)
-    print('accuracy is ', score)
-    if score > best_pred_score:
-        best_pred_score = score
-        best_classifer = c
-
-print('best classifier is ', best_classifer)
 X_kaggle = None
 Y_kaggle = None
 if 'X_kaggle.pickle' in os.listdir():
@@ -98,17 +73,52 @@ else:
     pickle.dump(X_train, open('X_kaggle.pickle','wb'))
     pickle.dump(Y_train, open('Y_kaggle.pickle','wb'))
 
-kaggle_pred = best_classifer.predict(X_kaggle)
+
+hyper_parameters = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+param_result = defaultdict(list)
+kf = KFold(n_splits=5, shuffle=True)
+kf.get_n_splits(X_train)
+
+print('Hyper parameter tuning')
+for train_index, test_index in kf.split(X_train):
+    x_train, x_test = X_train[train_index], X_train[test_index]
+    y_train, y_test = Y_train[train_index], Y_train[test_index]
+    for est in hyper_parameters:
+        rf = RandomForestClassifier(n_estimators=est)
+        rf.fit(x_train, y_train)
+        y_pred = rf.predict(x_test)
+        score = accuracy_score(y_test, y_pred)
+        param_result[est].append(score)
+print('Hyper parameter tuning Done')
+
+param_avg = {k:np.mean(param_result[k]) for k in param_result.keys()}
+best_param = max(param_avg, key=param_avg.get)
+print('best param', best_param)
+
+testing_score = []
+best_rf = None
+print('Getting testing score for all hyper parameters')
+for est in hyper_parameters:
+    rf = RandomForestClassifier(n_estimators=est)
+    rf.fit(X_train, Y_train)
+    y_pred = rf.predict(X_test)
+    score = accuracy_score(Y_test, y_pred)
+    testing_score.append(score)
+    print(est, score)
+    if est == best_param:
+        best_rf = rf
+
+print('plotting result')
+plt_labels = ['Train', 'Test']
+plt.plot(hyper_parameters, list(param_avg.values()),'or-')
+plt.plot(hyper_parameters, testing_score,'sb-')
+plt.grid(True)
+plt.ylabel('Accuracy')
+plt.xlabel('n_estimators')
+plt.title('Random Forest Hyper Parameter Tuning Result')
+plt.legend(plt_labels)
+plt.savefig('result.png')
+plt.close()
+
+kaggle_pred = best_rf.predict(X_kaggle)
 kaggleize(kaggle_pred, 'kaggle.csv')
-# kf = KFold(n_splits=5, shuffle=True)
-# kf.get_n_splits(X_train)
-#
-# print('KFold')
-# for c in classfiers:
-#     print('fitting data on ', c)
-#     for train_index, test_index in kf.split(X_train):
-#         X_train, X_test = X_train[train_index], X_train[test_index]
-#         y_train, y_test = Y_train[train_index], Y_train[test_index]
-#         c.fit(X_train, y_train)
-#         y_pred = c.predict(X_test)
-#         print(classification_report(y_test, y_pred, target_names=['1', '-1']))
